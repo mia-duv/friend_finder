7bb442ea27c3558ad74b6d6f320d9da37e21f791
mia-duv <56006479+mia-duv@users.noreply.github.com>
2020-02-04 23:19:33 -0500

    adding files and all code

---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+var friends = [ {
+    "name" : "Wish Bear",
+    "photo" : "https://www.vivagifts.com.au/images/wish2.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Bedtime Bear",
+    "photo" : "https://carebearscdn.blob.core.windows.net/carebearassets/wp-content/uploads/2018/10/Bedtime-1.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Tenderheart Bear",
+    "photo" : "https://d1wwra234reihl.cloudfront.net/art/ff/bc82606e8811e597ef73df71344f20/static/xlarge.jpg",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Birthday Bear",
+    "photo" : "https://i.pinimg.com/originals/58/3a/cc/583acccdb8fdf32f51dcde1bfcbd57c2.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Funshine Bear",
+    "photo" : "https://i.pinimg.com/originals/dc/94/77/dc9477bb3ab8765ec9073066b172a07a.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Cheer Bear",
+    "photo" : "https://www.cs.iusb.edu/~yul/C151/project/c/c151_images/low-cheer-care-bear.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Love A Lot Bear",
+    "photo" : "https://balloonwarehouse.com/shop/wp-content/uploads/2016/09/602987-big.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Grumpy Bear",
+    "photo" : "https://i.etsystatic.com/6315841/r/il/c2b51c/1179829448/il_570xN.1179829448_hyym.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Friend Bear",
+    "photo" : "https://i.pinimg.com/236x/ea/93/c0/ea93c0ceb4433c450caa7f68b7d9d239--care-bears-gif.jpg",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Good Luck Bear",
+    "photo" : "https://i.pinimg.com/originals/e2/b6/f9/e2b6f9e946907cb08f85d3bfe6639fe5.jpg",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+
+}];
